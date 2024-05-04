6777258733da30f3e80d66d4eb0a0b14d85d1977
Syaifuddin091 <syaifuddin091@gmail.com>
2024-05-04 14:59:49 +0700

    first-comit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <div id="app">
+    <p :class="{ 'highlight': isHighlighted }" :style="{ 'font-weight': isBold ? 'bold' : 'normal' }">Ini adalah teks dengan class dan style bindings</p>
+    <input type="text" v-model="inputText" @input="onInput">
+    <p>Data input: {{ inputText }}</p>
+  </div>
+  </template>
+
+<script setup>
+    import { ref } from 'vue'
+
+    const inputText = ref('');
+    const isHighlighted = ref(false);
+    const isBold = ref(false);
+    const Upercase = ref(false)
+
+    const onInput = () => {
+        isHighlighted.value = inputText.value.length > 5;
+        isBold.value = inputText.value.length > 10;
+    };
+</script>
+
+<style scoped>
+
+</style>
